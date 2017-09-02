7df7312543f4c89dedb43afca4d19e9edcbac704
Lukasz <ljablona@gmail.com>
2017-09-02 09:51:29 +0200

    Commmt1

--- src/Point.java
+++ src/Point.java
@@ -2,5 +2,28 @@
 public class Point {
 	private int id;
 	private double x,y;
+	public Point (int id, double x, double y){
+		this.id=id;
+		this.x=x;
+		this.y=y;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
 	
 }
